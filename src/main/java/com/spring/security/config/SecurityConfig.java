8654041446fed30e64a01ed28077eb2a158c6741
MG <min0903@adcapsule.co.kr>
2020-10-20 18:00:41 +0900

    커밋 테스트

--- src/main/java/com/spring/security/config/SecurityConfig.java
+++ src/main/java/com/spring/security/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.spring.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -10,10 +11,58 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.thymeleaf.engine.IterationStatusVar;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    @Bean
+
+  /*  @Bean
+    @Override
+    public UserDetailsService userDetailsService() {
+
+        //User Role
+        UserDetails theUser = User.withUsername("sergey")
+                .passwordEncoder(PasswordEncoderFactories.createDelegatingPasswordEncoder()::encode)
+                .password("1234").roles("USER").build();
+
+        //Manager Role
+        UserDetails theManager = User.withUsername("john")
+                .passwordEncoder(PasswordEncoderFactories.createDelegatingPasswordEncoder()::encode)
+                .password("1234").roles("MANAGER").build();
+
+        InMemoryUserDetailsManager userDetailsManager = new InMemoryUserDetailsManager();
+        userDetailsManager.createUser(theUser);
+        userDetailsManager.createUser(theManager);
+
+        return userDetailsManager;
+    }*/
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("min")
+                .password("{noop}1234")
+                .roles("USER")
+                .and()
+                .withUser("min2")
+                .password("{noop}1234")
+                .roles("MANAGER");
+    }
+/*
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/managers/status/check").hasAnyAuthority("DELETE_USER_AUTHORITY")
+                .antMatchers("/users/status/check").hasRole("USER")
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic()
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }*/
+
+   /*@Bean
     @Override
     public UserDetailsService userDetailsService() {
         UserDetails theUser = User.withUsername("min")
@@ -30,7 +79,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         userDetailsManager.createUser(theManager);
 
         return userDetailsManager;
-    }
+    }*/
 
     /*@Override
     protected void configure(HttpSecurity http) throws Exception {
